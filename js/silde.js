2c6f338d499024a5cac8c8ab05b5c36a9ffb9c8d
Carlos-Eduardo5Qs <carloseduardorm@outlook.com>
2022-07-15 00:46:44 -0300

    Adicionando slides de projetos não finalizado

---
+++ js/silde.js
@@ -0,0 +1,31 @@
+const control = document.querySelectorAll('.control');
+let currentItem = 0;
+const item = document.querySelectorAll('.item');
+const maxItem = item.length;
+
+control.forEach(control => {
+    control.addEventListener('click', () => {
+        const isLeft = control.classList.contains('arrow-left');
+        
+        if(isLeft) {
+            currentItem -= 1;
+        }else {
+            currentItem += 1;
+        }
+
+        if(currentItem > maxItem) {
+            currentItem = 0;
+        }
+
+        if(currentItem < 0) {
+            currentItem = maxItem -1;
+        }
+
+
+        item[currentItem].scrollIntoView({
+            inline:'center'
+        });
+    });
+});
+
+
